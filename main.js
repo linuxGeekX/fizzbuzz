ca82bcd59b0e48993bbf2860f84c3626da032047
Oscar Strookman <oscar.strookman@headforwards.com>
2016-12-19 10:02:11 +0000

    Writing out result to file fizzbuzz.txt.

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 (function() {
+	var fs = require('fs');
 	var fbArr = [];
 	for(var i = 1; i < 101; ++i) {
 		var str = i + ": ";
@@ -8,8 +9,9 @@
 			str += "buzz"
 		fbArr.push(str);
 	}
-	//console.log(fbArr);
-	fbArr.forEach(function(e) {
-		console.log(e);
+	console.log(fbArr);
+	fs.writeFile("fizzbuzz.txt", fbArr, "utf8", function(err) {
+		if(err !== null)
+			console.log('ERROR: ' + err);
 	});
 })();
