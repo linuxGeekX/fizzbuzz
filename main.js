e3a6cae5756faae253dc1c8db34be5701f75e4fb
Oscar Strookman <oscar.strookman@headforwards.com>
2016-12-19 12:43:45 +0000

    Now checks for file and reads it if it can.

--- main.js
+++ main.js
@@ -1,16 +1,34 @@
 (function() {
 	var fs = require('fs');
 	var fbArr = [];
-	for(var i = 1; i < 101; ++i) {
-		var str = i + ": ";
-		if(i % 3 == 0)
-			str += "fizz"
-		if(i % 5 == 0)
-			str += "buzz"
-		fbArr.push(str);
-	}
-	fs.writeFile("fizzbuzz.txt", fbArr, "utf8", function(err) {
+
+	fs.readFile("fizzbuzz.txt", "utf8", function(err, data) {
 		if(err !== null)
-			console.log('ERROR: ' + err);
+			generateFBData();
+		else {
+			console.log("Fetched array")
+			fbArr = JSON.parse(data);
+			writeFBData();
+		}
 	});
+
+	function generateFBData() {
+		console.log("Building array...")
+		for(var i = 1; i < 101; ++i) {
+			var str = i + ": ";
+			if(i % 3 == 0)
+				str += "fizz"
+			if(i % 5 == 0)
+				str += "buzz"
+			fbArr.push(str);
+		}
+		writeFBData();
+	}
+
+	function writeFBData() {
+		fs.writeFile("fizzbuzz.txt", JSON.stringify(fbArr), "utf8", function(err) {
+			if(err !== null)
+				console.log('ERROR: ' + err);
+		});
+	}
 })();
